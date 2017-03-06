7a40fb65593c7cb643143ead679a186286ece37d
M. Bermon Painter <bermonpainter@gmail.com>
2017-03-06 14:57:32 -0500

    Updated the JS to change the square on load (for the edit page)

--- MegaEscritorioRazor/assets/js/main.js
+++ MegaEscritorioRazor/assets/js/main.js
@@ -1,17 +1,31 @@
-$(document).ready(function() {
-    $(".material").change(function () {
-        var material = "";
-        $(".img-container ").removeClass("selected-border");
-        material = $(".material option:selected").val();
-        $("." + material).parent().addClass("selected-border");
-    });
-    
-    $(".img-container").click(function () {
-        $(".material").find('option:selected').removeAttr("selected");
-        var newValue = "";
-        $(".img-container ").removeClass("selected-border");
-        $(this).addClass("selected-border");
-        newValue = $(this).find('img:first').attr("class")
-        $(".material").find('option[value="' + newValue + '"]').attr("selected", true);
-    });
+$(document).ready(function () {
+    var materialDropdown = $(".material"),
+        
+
+    if (materialDropdown.length > 0) {
+        // Get the value of the material dropdown when the page loads
+        materialDropdownValue = materialDropdown.val();
+
+        // Set the correct square of the material image when the page loads 
+        $('.img-container').find("." + materialDropdownValue).addClass("selected-border");
+
+
+        // Change the image container when selecting a material from the dropdown
+        $(".material").change(function () {
+            var material = "";
+            $(".img-container ").removeClass("selected-border");
+            material = $(".material option:selected").val();
+            $("." + material).parent().addClass("selected-border");
+        });
+
+        // Change the material dropdown based on the material square
+        $(".img-container").click(function () {
+            $(".material").find('option:selected').removeAttr("selected");
+            var newValue = "";
+            $(".img-container ").removeClass("selected-border");
+            $(this).addClass("selected-border");
+            newValue = $(this).find('img:first').attr("class")
+            $(".material").find('option[value="' + newValue + '"]').attr("selected", true);
+        });
+    }
 });
