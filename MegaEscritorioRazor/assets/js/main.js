1ba907f90f60effd9f5df31fbc5cf9133dc1c9b3
M. Bermon Painter <bermonpainter@gmail.com>
2017-03-06 15:05:12 -0500

    Forgot the var

--- MegaEscritorioRazor/assets/js/main.js
+++ MegaEscritorioRazor/assets/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     if ($(".material").length) {
         // Get the value of the material dropdown when the page loads
-        materialDropdownValue = $(".material").val();
+        var materialDropdownValue = $(".material").val();
 
         // Set the correct square of the material image when the page loads 
         $('.img-container').find("." + materialDropdownValue).addClass("selected-border");
